017dd9b765a44034616d13d8b38775bab11d2ae0
Yash Mehta <yashmehta0395@gmail.com>
2025-02-20 12:51:01 +0530

    added ui components using shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
    <div>
     <p className="font-bold text-rose-500">
       Hello World
+      <Button variant="destructive" size="sm">
+        Click me
+      </Button>
     </p>
    </div>
   );
