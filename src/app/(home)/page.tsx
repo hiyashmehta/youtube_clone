622fd175d86e6edb44ca1c553e1b802f596a5ff4
Yash Mehta <yashmehta0395@gmail.com>
2025-02-23 20:30:32 +0530

    layout setup done, bun and package-lock removed, pnpm-lock added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
    <div>
-    <p className="font-bold text-rose-500">
-      Hello World
-      <Button variant="destructive" size="sm">
-        Click me
-      </Button>
-    </p>
+      I will upload videos in the future.
    </div>
   );
-}
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist_Mono, Inter } from "next/font/google";
 
 import "./globals.css";
 
