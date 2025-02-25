e4f2fba7d9d810d9f1060e47608fa7a711cc265f
Yash Mehta <yashmehta0395@gmail.com>
2025-02-25 11:17:51 +0530

    auth button component updated

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,16 +1,26 @@
+"use client";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button"
 import { UserCircleIcon } from "lucide-react"
 
 export const AuthButton = () => {
-    // TODO: Add different auth states
-    
     return (
-        <Button
-            variant="outline"
-            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500 rounded-full shadow-none-"
-        >
-            <UserCircleIcon/>
-            Sign In
-        </Button>
+        <>
+        <SignedIn>
+            <UserButton/>
+            {/* Add menu items for Studio and user profile */}
+        </SignedIn>
+            <SignedOut>
+                <SignInButton mode="modal" >
+                    <Button
+                        variant="outline"
+                        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500 rounded-full shadow-none-"
+                    >
+                        <UserCircleIcon/>
+                        Sign In
+                    </Button>
+                </SignInButton>
+            </SignedOut>
+        </>
     )
 }
