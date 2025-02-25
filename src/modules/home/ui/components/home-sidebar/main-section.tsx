e6826e7a2b9bbad368cdea1b743c49662812999e
Yash Mehta <yashmehta0395@gmail.com>
2025-02-25 11:21:29 +0530

    updated the main section and personal  section components integrated with clerk auth

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -3,6 +3,7 @@
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 import Link from "next/link";
+import { SignedIn, useAuth, useClerk } from "@clerk/nextjs";
 
 const items = [
     {
@@ -24,6 +25,9 @@ const items = [
 ];
 
 export const MainSection = () => {
+    const {  isSignedIn } = useAuth();
+    const clerk = useClerk();
+
     return (
         <SidebarGroup>
             <SidebarGroupContent>
@@ -34,7 +38,12 @@ export const MainSection = () => {
                                 tooltip={item.title}
                                 asChild
                                 isActive={false}       // TODO: change to look at current pathname
-                                onClick={() => {}}      // TODO: something on clock
+                                onClick={(e) => {
+                                    if (!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        return clerk.openSignIn();
+                                    }
+                                }}
                             >
                                 <Link href={item.url} className="flex items-center gap-4">
                                     <item.icon/>
