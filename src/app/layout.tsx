aebd03a05c90d1dd95e35272c8324545cbb64454
Yash Mehta <yashmehta0395@gmail.com>
2025-03-05 12:31:16 +0530

    created video schema, studio procedures and added video record creation, pushed database changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 import { TRPCProvider } from "@/trpc/client";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<TRPCProvider>
+					<Toaster/>
 				{children}
 				</TRPCProvider>
 			</body>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { studioRouter } from '@/modules/studio/server/procedures';
 
 import { createTRPCRouter } from "../init";
+import { videosRouter } from '@/modules/videos/server/procedures';
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
     studio: studioRouter,
+    videos: videosRouter,
 });
 
 // export type definition of API
