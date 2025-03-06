abec6c07b2704d44a1dedde0f1f37828a241d766
Yash Mehta <yashmehta0395@gmail.com>
2025-03-06 16:46:25 +0530

    updated the video thumbnail component and provided the video thumbnail and preview urls

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Link from "next/link";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
 
 
 export const VideosSection = () => {
@@ -46,7 +47,16 @@ export const VideosSectionSuspense = () => {
                             <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                                 <TableRow className="cursor-pointer">
                                     <TableCell>
-                                        {video.title}
+                                        <div className="flex items-center gap-4">
+                                            <div className="relative aspect-video w-36 shrink-0">
+                                                <VideoThumbnail
+                                                 imageUrl={video.thumbnailUrl}
+                                                  previewUrl={video.previewUrl}
+                                                  title={video.title}
+                                                  duration={video.duration ?? 0}
+                                                />
+                                            </div>
+                                        </div>
                                     </TableCell>
                                     <TableCell>
                                         visibility
