30c7459f3059a265869498e860bbd8cb9f463717
Yash Mehta <yashmehta0395@gmail.com>
2025-03-06 13:15:16 +0530

    created video upload modal

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 // import { TRPCError } from "@trpc/server";
 
@@ -9,16 +10,28 @@ export const videosRouter = createTRPCRouter({
 
         // throw new TRPCError({ code: "BAD_REQUEST", message: "Not implemented" });
 
+        const upload = await mux.video.uploads.create({
+            new_asset_settings: {
+                passthrough: userId,
+                playback_policy: ["public"],
+                mp4_support: "standard",
+            },
+            cors_origin: '*',
+        });
+
         const [video] = await db
             .insert(videos)
             .values({
                 userId,
                 title: "untitled",
+                muxStatus: "waiting",
+                muxUploadId: upload.id,
             })
             .returning();
 
             return {
                 video: video,
+                url: upload.url,
             };
     }),
 });
