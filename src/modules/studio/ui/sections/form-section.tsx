22e23c4af1ce27096ccc921ab93dafbab60fccb6
Yash Mehta <yashmehta0395@gmail.com>
2025-03-08 20:27:23 +0530

    created video player component and added it to the form section

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -32,6 +32,7 @@ import {
 import { z } from "zod";
 import { videoUpdateSchema } from "@/db/schema";
 import { toast } from "sonner";
+import { VideoPlayer } from "@/modules/videos/ui/components/video-player";
 
 interface FormSectionProps {
     videoId: string;
@@ -176,6 +177,16 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
                                 </FormItem>
                             )}
                         />
+                    </div>
+                    <div className="flex flex-col gap-y-8 lg:col-span-2">
+                        <div className="flex flex-col gap-4 bg-[#f9f9f9] rounded-xl overflow-hidden h-fit">
+                            <div className="aspect-video overflow-hidden relative">
+                                <VideoPlayer
+                                    playbackId={video.muxPlaybackId}
+                                    thumbnailUrl={video.thumbnailUrl}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </form>
