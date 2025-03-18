f607560af330413f085fcbe4b023e2d522f8428f
Yash Mehta <yashmehta0395@gmail.com>
2025-03-18 15:02:32 +0530

    created the video page

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
+import { VideoBanner } from "../components/video-banner";
+import { VideoTopRow } from "../components/video-top-row";
 
 interface VideoSectionProps {
     videoId: string;
@@ -22,8 +26,20 @@ const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
     const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
 
     return (
-        <div>
-            {JSON.stringify(video)}
-        </div>
+        <>
+            <div className={cn(
+                "aspect-video bg-black rounded-xl overflow-hidden relative",
+                video.muxStatus !== "ready" && "rounded-b-none",
+            )}>
+                <VideoPlayer
+                    autoPlay
+                    onPlay={() => {}}
+                    playbackId={video.muxPlaybackId}
+                    thumbnailUrl={video.thumbnailUrl}
+                />
+            </div>
+            <VideoBanner status={video.muxStatus} />
+            <VideoTopRow video={video} />
+        </>
     )
 };
