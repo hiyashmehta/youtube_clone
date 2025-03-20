c7f33d1f46ddbff91da50fcf11444ef78bda29fb
Yash Mehta <yashmehta0395@gmail.com>
2025-03-20 16:35:35 +0530

    created video views router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { studioRouter } from '@/modules/studio/server/procedures';
 
 import { createTRPCRouter } from "../init";
 import { videosRouter } from '@/modules/videos/server/procedures';
+import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
     studio: studioRouter,
     videos: videosRouter,
+    videoViews: videoViewsRouter,
 });
 
 // export type definition of API
