fb357352b9906f754a3e545ae7178def482cbb4c
Yash Mehta <yashmehta0395@gmail.com>
2025-03-21 17:45:15 +0530

    created subscriptions router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,6 +5,7 @@ import { createTRPCRouter } from "../init";
 import { videosRouter } from '@/modules/videos/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
+import { subscriptionsRouter } from '@/modules/subscriptions/server/procedures';
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
     videos: videosRouter,
     videoViews: videoViewsRouter,
     videoReactions: videoReactionsRouter,
+    subscriptions: subscriptionsRouter,
 });
 
 // export type definition of API
