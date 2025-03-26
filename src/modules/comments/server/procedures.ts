091ea14dc8e0b5f692223b2147387c64872b64c9
Yash Mehta <yashmehta0395@gmail.com>
2025-03-26 12:19:42 +0530

    Added remove procedure to the comment section

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -3,8 +3,32 @@ import { baseProcedure, createTRPCRouter, protectedProcedure } from '@/trpc/init
 import { db } from '@/db';
 import { comments, users } from '@/db/schema';
 import { and, count, desc, eq, getTableColumns, lt, or } from 'drizzle-orm';
+import { TRPCError } from '@trpc/server';
 
 export const commentsRouter = createTRPCRouter({
+    remove: protectedProcedure
+        .input(z.object({
+            id: z.string().uuid(),
+        }))
+        .mutation(async ({ input, ctx }) => {
+            const { id } = input;
+            const { id: userId } = ctx.user;
+
+            const [deletedComment] = await db
+                .delete(comments)
+                .where(and(
+                    eq(comments.id, id),
+                    eq(comments.userId, userId),
+                ))
+                .returning();
+
+            if(!deletedComment) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }
+
+            return deletedComment;
+        }),
+
     create: protectedProcedure
         .input(z.object({
             videoId: z.string().uuid(),
