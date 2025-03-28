2fcf9385340425a669a8f4671583fe985cd96d83
Yash Mehta <yashmehta0395@gmail.com>
2025-03-28 16:16:20 +0530

    created comments reactions schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { reactionType } from './schema';
 import { relations } from "drizzle-orm";
 import { integer, pgEnum, pgTable, primaryKey, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 import {
@@ -5,6 +6,8 @@ import {
     createSelectSchema,
     createUpdateSchema,
 } from "drizzle-zod";
+
+export const reactionType = pgEnum("reaction_type", ["like", "dislike"]);
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -139,6 +142,19 @@ export const commentSelectSchema = createSelectSchema(comments);
 export const commentInsertSchema = createInsertSchema(comments);
 export const commentUpdateSchema = createUpdateSchema(comments);
 
+export const commentReactions = pgTable('comment_reactions', {
+    userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+    commentId: uuid("comment_id").references(() => comments.id, { onDelete: "cascade" }).notNull(),
+    type: reactionType("type").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => [
+    primaryKey({
+        name: "comment_reactions_pk",
+        columns: [t.userId, t.commentId],
+    }),
+])
+
 export const videoViews = pgTable("video_views", {
     userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
     videoId: uuid("video_id").references(() => videos.id, { onDelete: "cascade" }).notNull(),
@@ -166,7 +182,7 @@ export const videoViewSelectSchema = createSelectSchema(videoViews);
 export const videoViewInsertSchema = createInsertSchema(videoViews);
 export const videoViewUpdateSchema = createUpdateSchema(videoViews);
 
-export const reactionType = pgEnum("reaction_type", ["like", "dislike"]);
+
 
 export const videoReactions = pgTable("video_reactions", {
     userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
