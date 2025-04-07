00f865f8e575bbcd364a97137f5626b581a312a8
Yash Mehta <yashmehta0395@gmail.com>
2025-04-07 12:29:02 +0530

    updated suggestions section for video views

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,7 +1,25 @@
-export const SuggestionsSection = () => {
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+
+interface SuggestionsSectionProps {
+    videoId: string;
+};
+
+export const SuggestionsSection = ({
+    videoId
+}: SuggestionsSectionProps) => {
+    const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery({
+        videoId,
+        limit: DEFAULT_LIMIT,
+    }, {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+    });
+
     return (
         <div>
-            Suggestions
+            {JSON.stringify(suggestions)}
         </div>
     )
 }
