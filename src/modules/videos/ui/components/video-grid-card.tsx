440d3c463c3d0510c4d50a3b77b575205b78725f
Yash Mehta <yashmehta0395@gmail.com>
2025-04-10 13:15:02 +0530

    added skeleton to video grid card component

--- src/modules/videos/ui/components/video-grid-card.tsx
+++ src/modules/videos/ui/components/video-grid-card.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
-import { VideoThumbnail } from "./video-thumbnail";
+import { VideoThumbnail, VideoThumbnailSkeleton } from "./video-thumbnail";
 import { VideoGetManyOutput } from "../../types";
-import { VideoInfo } from "./video-info";
+import { VideoInfo, VideoInfoSkeleton } from "./video-info";
 
 interface VideoGridCardProps {
     data: VideoGetManyOutput["items"][number];
     onRemove?: () => void;
 }
+
+export const VideoGridCardSkeleton = () => {
+    return (
+        <div className="flex flex-col gap-2 w-full">
+            <VideoThumbnailSkeleton/>
+            <VideoInfoSkeleton/>
+        </div>
+    );
+};
 
 export const VideoGridCard = ({
     data,
