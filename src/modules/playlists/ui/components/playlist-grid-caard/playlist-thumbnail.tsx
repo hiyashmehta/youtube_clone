b82ab5b7f78d87e20409ac2f7696d847a4e07d0e
Yash Mehta <yashmehta0395@gmail.com>
2025-04-18 12:56:50 +0530

    updated playlist thumbnail and added skeleton

--- src/modules/playlists/ui/components/playlist-grid-caard/playlist-thumbnail.tsx
+++ src/modules/playlists/ui/components/playlist-grid-caard/playlist-thumbnail.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { THUMBNAIL_FALLBACK } from "@/modules/videos/constants";
 import { ListVideoIcon, PlayIcon } from "lucide-react";
@@ -9,6 +10,14 @@ interface PlaylistThumbnailProps {
     videoCount: number;
     className?: string;
     imageUrl?: string | null;
+};
+
+export const PlaylistThumbnailSkeleton = () => {
+    return (
+        <div className="relative w-full overflow-hidden rounded-xl aspect-video">
+            <Skeleton className="size-full" />
+        </div>
+    );
 };
 
 export const PlaylistThumbnail = ({
