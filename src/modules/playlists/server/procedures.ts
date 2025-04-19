03fab5e3cee511fbd8d53974454eb0ed8368500d
Yash Mehta <yashmehta0395@gmail.com>
2025-04-19 13:46:02 +0530

    created procedures for adding and removing video from playlists

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -7,6 +7,117 @@ import {  createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { and, desc, eq, getTableColumns, lt, or, sql } from "drizzle-orm";
 
 export const playlistsRouter = createTRPCRouter({
+    removeVideo: protectedProcedure
+    .input(z.object({
+        playlistId: z.string().uuid(),
+        videoId: z.string().uuid(),
+    }))
+    .mutation(async ({ input, ctx }) => {
+        const { playlistId, videoId  } = input;
+        const { id: userId } = ctx.user;
+
+        const [existingPlaylist] = await db
+        .select()
+        .from(playlists)
+        .where(and(
+            eq(playlists.id, playlistId),
+            eq(playlists.userId, userId),
+        ));
+
+        if(!existingPlaylist) {
+            throw new TRPCError({ code: "NOT_FOUND" });
+        }
+
+        const [existingVideo] = await db
+        .select()
+        .from(videos)
+        .where(eq(videos.id, videoId));
+
+        if(!existingVideo) {
+            throw new TRPCError({ code: "NOT_FOUND" });
+        }
+
+        const [existingPlaylistVideo] = await db
+        .select()
+        .from(playlistVideos)
+        .where(
+            and(
+                eq(playlistVideos.playlistId, playlistId),
+                eq(playlistVideos.videoId, videoId),
+            )
+        );
+
+        if(!existingPlaylistVideo) {
+            throw new TRPCError({ code: "NOT_FOUND" });
+        }
+
+        const [deletedPlaylistVideo] = await db
+        .delete(playlistVideos)
+        .where(
+            and(
+                eq(playlistVideos.playlistId, playlistId),
+                eq(playlistVideos.videoId, videoId),
+            )
+        )
+        .returning();
+
+        return deletedPlaylistVideo;
+
+    }),
+
+    addVideo: protectedProcedure
+    .input(z.object({
+        playlistId: z.string().uuid(),
+        videoId: z.string().uuid(),
+    }))
+    .mutation(async ({ input, ctx }) => {
+        const { playlistId, videoId  } = input;
+        const { id: userId } = ctx.user;
+
+        const [existingPlaylist] = await db
+        .select()
+        .from(playlists)
+        .where(and(
+            eq(playlists.id, playlistId),
+            eq(playlists.userId, userId),
+        ));
+
+        if(!existingPlaylist) {
+            throw new TRPCError({ code: "NOT_FOUND" });
+        }
+
+        const [existingVideo] = await db
+        .select()
+        .from(videos)
+        .where(eq(videos.id, videoId));
+
+        if(!existingVideo) {
+            throw new TRPCError({ code: "NOT_FOUND" });
+        }
+
+        const [existingPlaylistVideo] = await db
+        .select()
+        .from(playlistVideos)
+        .where(
+            and(
+                eq(playlistVideos.playlistId, playlistId),
+                eq(playlistVideos.videoId, videoId),
+            )
+        );
+
+        if(existingPlaylistVideo) {
+            throw new TRPCError({ code: "CONFLICT" });
+        }
+
+        const [createdPlaylistVideo] = await db
+        .insert(playlistVideos)
+        .values({ playlistId, videoId })
+        .returning();
+
+        return createdPlaylistVideo;
+
+    }),
+
     getManyForVideo: protectedProcedure
     .input(
         z.object({
