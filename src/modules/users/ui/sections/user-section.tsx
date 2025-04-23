02929a56e38735f83969fab522ca41cb6e1a5d48
Yash Mehta <yashmehta0395@gmail.com>
2025-04-23 18:40:47 +0530

    added user page banner component to user section

--- src/modules/users/ui/sections/user-section.tsx
+++ src/modules/users/ui/sections/user-section.tsx
@@ -3,6 +3,7 @@
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { UserPageBanner } from "../components/user-page-banner";
 
 interface UserSectionProps {
     userId: string;
@@ -22,8 +23,8 @@ const UserSectionSuspense = ({ userId }: UserSectionProps) => {
     const [user] = trpc.users.getOne.useSuspenseQuery({ id: userId });
 
     return(
-        <div>
-            {JSON.stringify(user)}
+        <div className="flex flex-col">
+            <UserPageBanner user={user} />
         </div>
     )
 }
