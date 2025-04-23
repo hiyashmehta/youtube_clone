74259444e1c3bd477b0fb88b82a95fc066502972
Yash Mehta <yashmehta0395@gmail.com>
2025-04-23 17:19:13 +0530

    created getOne procedure for users

--- src/modules/users/server/procedures.ts
+++ src/modules/users/server/procedures.ts
@@ -1,89 +1,62 @@
 import { db } from "@/db";
 import { z } from "zod";
-import { subscriptions, users, videoReactions, videos, videoUpdateSchema, videoViews } from "@/db/schema";
-import { mux } from "@/lib/mux";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { subscriptions, users, videos } from "@/db/schema";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, desc, eq, getTableColumns, inArray, isNotNull, lt, or } from "drizzle-orm";
-import { UTApi } from "uploadthing/server";
-import { workflow } from "@/lib/workflow";
+import { eq, getTableColumns, inArray, isNotNull } from "drizzle-orm";
 
 export const usersRouter = createTRPCRouter({
-    getOne: baseProcedure
-    .input(z.object({ id: z.string().uuid() }))
-    .query(async ({ input, ctx  }) => {
-        const { clerkUserId } = ctx;
+	getOne: baseProcedure
+		.input(z.object({ id: z.string().uuid() }))
+		.query(async ({ input, ctx }) => {
+			const { clerkUserId } = ctx;
 
-        let userId;
+			let userId;
 
-        const [user] = await db
-            .select()
-            .from(users)
-            .where(inArray(users.clerkId, clerkUserId ? [clerkUserId] : []))
+			const [user] = await db
+				.select()
+				.from(users)
+				.where(
+					inArray(users.clerkId, clerkUserId ? [clerkUserId] : []),
+				);
 
-        if(user) {
-            userId = user.id;
-        }
-        
-        const viewerReactions = db.$with("viewer_reactions").as(
-            db
-                .select({
-                    videoId: videoReactions.videoId,
-                    type: videoReactions.type,
-                })
-                .from(videoReactions)
-                .where(inArray(videoReactions.userId, userId ? [userId] : []))
-        );
+			if (user) {
+				userId = user.id;
+			}
 
-        const viewerSubscriptions = db.$with("viewer_subscriptions").as(
-            db
-                .select()
-                .from(subscriptions)
-                .where(inArray(subscriptions.viewerId, userId ? [userId] : []))
-        )
+			const viewerSubscriptions = db.$with("viewer_subscriptions").as(
+				db
+					.select()
+					.from(subscriptions)
+					.where(
+						inArray(subscriptions.viewerId, userId ? [userId] : []),
+					),
+			);
 
-        const [existingVideo] = await db
-            .with(viewerReactions, viewerSubscriptions)
-            .select({
-                ...getTableColumns(videos),
-                user: {
-                    ...getTableColumns(users),
-                    subscriberCount: db.$count(subscriptions, eq(subscriptions.creatorId, users.id)), 
-                    viewerSubscribed: isNotNull(viewerSubscriptions.viewerId).mapWith(Boolean),
-                },
-                viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
-                likeCount: db.$count(
-                    videoReactions,
-                    and(
-                        eq(videoReactions.videoId, videos.id),
-                        eq(videoReactions.type, "like"),
-                    ),
-                ),
-                dislikeCount: db.$count(
-                    videoReactions,
-                    and(
-                        eq(videoReactions.videoId, videos.id),
-                        eq(videoReactions.type, "dislike"),
-                    ),
-                ),
-                viewerReaction: viewerReactions.type
-            })
-            .from(videos)
-            .innerJoin(users, eq(videos.userId, users.id))
-            .leftJoin(viewerReactions, eq(viewerReactions.videoId, videos.id))
-            .leftJoin(viewerSubscriptions, eq(viewerSubscriptions.creatorId, users.id))
-            .where(eq(videos.id, input.id))
-            // .groupBy(
-            //     videos.id,
-            //     users.id,
-            //     viewerReactions.type,
-            // )
-            ;
+			const [existingUser] = await db
+				.with(viewerSubscriptions)
+				.select({
+					...getTableColumns(users),
+					viewerSubscribed: isNotNull(
+						viewerSubscriptions.viewerId,
+					).mapWith(Boolean),
+					videoCount: db.$count(videos, eq(videos.userId, users.id)),
+					subscriberCount: db.$count(
+						subscriptions,
+						eq(subscriptions.creatorId, users.id),
+					),
+				})
+				.from(users)
+				.leftJoin(
+					viewerSubscriptions,
+					eq(viewerSubscriptions.creatorId, users.id),
+				)
+				.where(eq(users.id, input.id));
 
-        if(!existingVideo) {
-            throw new TRPCError({ code: "NOT_FOUND" });
-        }
+			if (!existingUser) {
+				throw new TRPCError({ code: "NOT_FOUND" });
+			}
 
-        return existingVideo;
-    }),
+			return existingUser;
+		}),
 });
