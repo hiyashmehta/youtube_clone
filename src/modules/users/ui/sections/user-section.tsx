5cf09500edd100fcc111f6309c7fd0ce0f253f83
Yash Mehta <yashmehta0395@gmail.com>
2025-04-24 17:49:07 +0530

    updated user section

--- src/modules/users/ui/sections/user-section.tsx
+++ src/modules/users/ui/sections/user-section.tsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { UserPageBanner, UserPageBannerSkeleton } from "../components/user-page-banner";
 import { UserPageInfo, UserPageInfoSkeleton } from "../components/user-page-info";
+import { Separator } from "@/components/ui/separator";
 
 interface UserSectionProps {
     userId: string;
@@ -22,8 +23,11 @@ export const UserSection = (props: UserSectionProps) => {
 
 const UserSectionSkeleton = () => {
     return (
+        <div className="flex flex-col">
         <UserPageBannerSkeleton/>
         <UserPageInfoSkeleton />
+        <Separator/>
+        </div>
     )
 }
 
@@ -34,6 +38,7 @@ const UserSectionSuspense = ({ userId }: UserSectionProps) => {
         <div className="flex flex-col">
             <UserPageBanner user={user} />
             <UserPageInfo user={user} />
+            <Separator/>
         </div>
     )
 }
