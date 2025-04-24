62eec03daa08b0d41a1b069c46ee55d462cc8d80
Yash Mehta <yashmehta0395@gmail.com>
2025-04-24 16:32:59 +0530

    created mobile layout of user page info component

---
+++ src/modules/users/ui/components/user-page-info.tsx
@@ -0,0 +1,68 @@
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { UserGetOneOutput } from "../../types";
+import { UserAvatar } from "@/components/user-avatar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { UseSubscription } from "@/modules/subscriptions/hooks/use-subscription";
+
+interface UserPageInfoProps {
+    user: UserGetOneOutput;
+}
+
+export const UserPageInfo = ({user}: UserPageInfoProps) => {
+    const { userId, isLoaded } = useAuth();
+    const clerk = useClerk();
+
+    const { isPending, onClick } = UseSubscription({
+        userId: user.id,
+        isSubscribed: user.viewerSubscribed,
+    })
+    
+    return (
+        <div className="py-6">
+            {/* Mobile Layout */}
+            <div className="flex flex-col md:hidden">
+                <div className="flex items-center gap-3">
+                    <UserAvatar
+                        size="lg"
+                        imageUrl={user.imageUrl}
+                        name={user.name}
+                        className= "h-[60px] w-[60px]"
+                        onClick={() => {
+                            if (user.clerkId === userId){
+                                clerk.openUserProfile();
+                            }
+                        }}
+                    />
+                    <div className="flex-1 min-w-0">
+                        <h1 className="text-xl font-bold">
+                            {user.name}
+                        </h1>
+                        <div className="flex items-center gap-1 text-xs text-muted-foreground mt-1">
+                            <span>{user.subscribersCount} subscribers</span>
+                            <span>&bull;</span>
+                            <span>{user.videoCount} videos</span>
+                        </div>
+                    </div>
+                </div>
+                {userId === user.clerkId ? (
+                    <Button
+                        variant="secondary"
+                        asChild
+                        className="w-full mt-3 rounded-full"
+                    >
+                        <Link href="/studio">Go to studio</Link>
+                    </Button>
+                ): (
+                    <SubscriptionButton
+                        disabled={isPending || isLoaded}
+                        isSubscribed={user.viewerSubscribed}
+                        onClick={onClick}
+                        className="w-full mt-3"
+                    />
+                )}
+            </div>
+        </div>
+    )
+}
