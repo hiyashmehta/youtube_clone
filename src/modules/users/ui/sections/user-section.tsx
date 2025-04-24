8dd4e4364aa5a49bb1ea6b47db36e01bf3650af3
Yash Mehta <yashmehta0395@gmail.com>
2025-04-24 17:36:51 +0530

    Added skeleton for user section

--- src/modules/users/ui/sections/user-section.tsx
+++ src/modules/users/ui/sections/user-section.tsx
@@ -3,8 +3,8 @@
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { UserPageBanner } from "../components/user-page-banner";
-import { UserPageInfo } from "../components/user-page-info";
+import { UserPageBanner, UserPageBannerSkeleton } from "../components/user-page-banner";
+import { UserPageInfo, UserPageInfoSkeleton } from "../components/user-page-info";
 
 interface UserSectionProps {
     userId: string;
@@ -12,13 +12,20 @@ interface UserSectionProps {
 
 export const UserSection = (props: UserSectionProps) => {
     return (
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<UserSectionSkeleton />}>
             <ErrorBoundary fallback={<p>Error</p>}>
                 <UserSectionSuspense {...props} />
             </ErrorBoundary>
         </Suspense>
     );
 };
+
+const UserSectionSkeleton = () => {
+    return (
+        <UserPageBannerSkeleton/>
+        <UserPageInfoSkeleton />
+    )
+}
 
 const UserSectionSuspense = ({ userId }: UserSectionProps) => {
     const [user] = trpc.users.getOne.useSuspenseQuery({ id: userId });
